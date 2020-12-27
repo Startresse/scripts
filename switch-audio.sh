8a400d2e70d462c4824ddfc87d594246552b71d5
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-27 10:28:13 +0100

    better logic and comment update on switch audio

--- switch-audio.sh
+++ switch-audio.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # get sinks and current sink
+current_sink_index=$(pacmd list-sinks | grep '*' | grep -o '[0-9]*')
 sink_index_list=( $(pacmd list-sinks | grep 'index' | grep -o '[0-9]*') )
 eval sink_name_list=( $(pacmd list-sinks | grep device.product.name | grep -o '".*"') )
-current_sink_index=$(pacmd list-sinks | grep '*' | grep -o '[0-9]*')
 
-# fond index of next unused sink
-next_index=0
+# find index of next unused sink
 for ((i=0;i<${#sink_index_list[@]};i++)) do
-    if [ $device_found ]; then
-        next_index=$i
-        break
-    fi
-
     if [ ${sink_index_list[$i]} -eq $current_sink_index ]; then
-        device_found=1
+        next_index=$((($i + 1) % ${#sink_index_list[@]}))
+        break
     fi
 done
 
-# switch to next unused sink
+# default behaviour if problem in finding current sink
+if [ -z $next_index ]; then
+    next_index=0
+fi
+
+# switch to next unused sink and display name
 pacmd set-default-sink ${sink_index_list[$next_index]}
 echo Switched to : ${sink_name_list[$next_index]}
