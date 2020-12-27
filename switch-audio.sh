be95754c287f9fe44af48a70187604f1ef1a8c88
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-27 10:30:00 +0100

    removed useless condition

--- switch-audio.sh
+++ switch-audio.sh
@@ -5,6 +5,9 @@ current_sink_index=$(pacmd list-sinks | grep '*' | grep -o '[0-9]*')
 sink_index_list=( $(pacmd list-sinks | grep 'index' | grep -o '[0-9]*') )
 eval sink_name_list=( $(pacmd list-sinks | grep device.product.name | grep -o '".*"') )
 
+# default behaviour if problem in finding current sink
+next_index=0
+
 # find index of next unused sink
 for ((i=0;i<${#sink_index_list[@]};i++)) do
     if [ ${sink_index_list[$i]} -eq $current_sink_index ]; then
@@ -12,11 +15,6 @@ for ((i=0;i<${#sink_index_list[@]};i++)) do
         break
     fi
 done
-
-# default behaviour if problem in finding current sink
-if [ -z $next_index ]; then
-    next_index=0
-fi
 
 # switch to next unused sink and display name
 pacmd set-default-sink ${sink_index_list[$next_index]}
