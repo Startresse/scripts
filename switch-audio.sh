d3e236aa3d3406037cbb8d72a993520702c86444
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-27 10:13:04 +0100

    switch-audio 2.0

--- switch-audio.sh
+++ switch-audio.sh
@@ -2,7 +2,7 @@
 
 # get sinks and current sink
 sink_index_list=( $(pacmd list-sinks | grep 'index' | grep -o '[0-9]*') )
-sink_name_list=( $(pacmd list-sinks | grep device.product.name | grep -o '".*"' | sed 's/\ //g') )
+eval sink_name_list=( $(pacmd list-sinks | grep device.product.name | grep -o '".*"') )
 current_sink_index=$(pacmd list-sinks | grep '*' | grep -o '[0-9]*')
 
 # fond index of next unused sink
@@ -20,4 +20,4 @@ done
 
 # switch to next unused sink
 pacmd set-default-sink ${sink_index_list[$next_index]}
-echo Switched to ${sink_name_list[$next_index]}
+echo Switched to : ${sink_name_list[$next_index]}
